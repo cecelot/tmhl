bee1e0f1757b440caa67d7641e32529988c5e8f3
Sydney <68250402+cecelot@users.noreply.github.com>
2024-09-30 14:08:42 -0400

    chore: init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          texlive.combined.scheme-full
+          texlab
+        ];
+      };
+    });
+}
